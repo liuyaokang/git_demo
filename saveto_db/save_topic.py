e39d46465a8cfd54c5e7bfbef45786b60a04a0ad
“liuyaokang” <804121415@qq.com>
2023-02-14 17:16:16 +0800

    保存到数据库

--- saveto_db/save_topic.py
+++ saveto_db/save_topic.py
@@ -1,5 +1,7 @@
-from modles import Topic
+from modles import Topic,Answer,Author
 from datetime import datetime
+
+
 def extract_topic(data_list):
     for value in data_list:
         # 解析user和topic入库
@@ -7,19 +9,21 @@ def extract_topic(data_list):
         topic = Topic()
         topic.id = content["contentId"]
         topic.title = content["topicTitle"]
-        topic.content = content["description"]
+        topic.content = content['description']
+        if topic.content == None:
+            topic.content = '暂无数据'
+        topic.last_answer_time = datetime.strptime(content['updateTime'], '%Y-%m-%d %H:%M:%S')
         topic.create_time = datetime.strptime(content["createTime"], '%Y-%m-%d %H:%M:%S')
-        # topic.create_time = datetime.strptime(content["createTime"], '%Y-%m-%d %H:%M:%S')
         topic.answer_nums = content["commentCount"]
         topic.click_nums = content["viewCount"]
         topic.praised_nums = content["diggNum"]
         topic.author = content["username"]
 
-        topic.save()
-        # pass
         existed_topics = Topic.select().where(Topic.id == topic.id)
         if existed_topics:
             topic.save()
         else:
             topic.save(force_insert=True)
+        pass
         # parse_topic(content["url"])
+        # parse_author(f'https://blog.csdn.net/{content["username"]}')
