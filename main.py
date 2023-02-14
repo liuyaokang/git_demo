1c1309e9d219f3d3726d35ebe1c4828ddbc32a04
“liuyaokang” <804121415@qq.com>
2023-02-14 14:46:56 +0800

    入口函数

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from get_data import parase_list
+from get_urls import get_last_urls
+
+
+
+if __name__ == "__main__":
+    urls  = get_last_urls()
+    u = urls
+    for url in urls:
+        parase_list(url)
+        break
 
