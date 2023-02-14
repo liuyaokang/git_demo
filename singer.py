4250a1788473edd76427ab2ae7dfe86e55befadc
“liuyaokang” <804121415@qq.com>
2023-02-14 14:28:52 +0800

    获取数据

--- singer.py
+++ singer.py
@@ -1,35 +1,11 @@
 import requests
-from scrapy import Selector #使用scrapy的Selector解析html
-from csdn import Signer
+
+from get_urls import get_last_urls
 import re
 import json
 from urllib.parse import urlparse,parse_qs
+from save_topic import extract_topic
 
-def get_last_urls():
-    urls = []
-    headers ={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-    }
-    rsp = requests.get("https://bbs.csdn.net/forums/activity",headers=headers)
-    if rsp.status_code !=200:
-        raise Exception("反爬了")
-    '''获取社区一级目录下的html'''
-    sel = Selector(text=rsp.text)
-    c_nodes = sel.css("div.custom-tree-node")
-    for index,c_node in enumerate(c_nodes):
-        url = "https://bizapi.csdn.net/community-cloud/v1/homepage/community/by/tag?deviceType=PC&tagId={}".format(index+1)
-        '''一级目录的url'''
-        singer = Signer()
-        code,re_json = singer.get_html(url)
-        if code!=200:
-            raise Exception("反爬了")
-        '''根据singer中的反反爬函数去抓取一级页面的数据，取一级目录下二级目录的'''
-        if  "data" in re_json:
-            for item in re_json["data"]:
-                url = "{}?category={}".format(item['url'],item['id'])
-                urls.append(url)
-        break
-    return urls
 
 def parase_list(url):
     next_page = 1
@@ -49,10 +25,8 @@ def parase_list(url):
     if data:
         data_str =data.group()
         data_json = json.loads(data_str)
-        total = data_json["pageData"]["data"]["baseInfo"]["page"]["total"]
-        current_page = data_json["pageData"]["data"]["baseInfo"]["page"]["currentPage"]
-        page_size = data_json["pageData"]["data"]["baseInfo"]["page"]["pageSize"]
-        tabid = data_json["pageData"]["data"]["baseInfo"]['defaultActiveTab']
+        data_list = data_json["pageData"]["data"]["baseInfo"]["dataList"]
+    extract_topic(data_json["pageData"]["data"]["baseInfo"]["dataList"])
     return data
 
 if __name__ == "__main__":
@@ -60,3 +34,4 @@ if __name__ == "__main__":
     u = urls
     for url in urls:
         parase_list(url)
+        break
